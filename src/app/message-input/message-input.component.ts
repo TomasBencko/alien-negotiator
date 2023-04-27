ebb1712a6219c6eb11c1fc9a45d2d7d312260d9a
Tomáš Bencko <tomas.bencko@cassoviacode.com>
2023-04-27 16:16:30 +0200

    Polish scrolling behavior – 4th iteration

--- src/app/message-input/message-input.component.ts
+++ src/app/message-input/message-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-message-input',
@@ -7,10 +7,12 @@ import { Component, EventEmitter, Output } from '@angular/core';
 })
 export class MessageInputComponent {
   @Output() sendMessage = new EventEmitter<string>();
+  @ViewChild('messageInput') messageInput!: ElementRef<HTMLInputElement>;
   inputMessage = '';
 
   onSendMessage() {
     this.sendMessage.emit(this.inputMessage);
     this.inputMessage = '';
+    this.messageInput.nativeElement.blur();
   }
 }
