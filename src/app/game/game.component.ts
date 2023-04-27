40faeda4810cb196570bceb7907a4fb03faf8491
Tomáš Bencko <tomas.bencko@cassoviacode.com>
2023-04-27 11:32:10 +0200

    Add a typing animation – 1st iteration

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -8,11 +8,19 @@ import { GameService } from '../game.service';
 })
 export class GameComponent {
   messages: { isUserMessage: boolean; text: string }[] = [];
+  isTyping: boolean = false;
 
   constructor(private gameService: GameService) {}
 
   async testAPI(inputMessage: string) {
     this.messages.push({ isUserMessage: true, text: inputMessage });
+
+    this.isTyping = true;
+    setTimeout(async () => {
+      const response = await this.gameService.sendMessageToAI(inputMessage);
+      this.messages.push({ isUserMessage: false, text: response.message });
+      this.isTyping = false;
+    }, 500);
 
     const response = await this.gameService.sendMessageToAI(inputMessage);
     this.messages.push({ isUserMessage: false, text: response.message });
