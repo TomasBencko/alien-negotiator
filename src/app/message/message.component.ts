4ac67a73f7f631875d15e7f388e1c0506f4282ee
Tomáš Bencko <tomas.bencko@cassoviacode.com>
2023-04-27 12:17:52 +0200

    Add text appearing animation

--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -1,11 +1,32 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-message',
   templateUrl: './message.component.html',
   styleUrls: ['./message.component.scss'],
 })
-export class MessageComponent {
+export class MessageComponent implements OnInit {
   @Input() isUserMessage!: boolean;
   @Input() messageText!: string;
+  displayedMessage = '';
+
+  ngOnInit() {
+    if (!this.isUserMessage) {
+      this.animateResponseText();
+    } else {
+      this.displayedMessage = this.messageText;
+    }
+  }
+
+  async animateResponseText() {
+    const words = this.messageText.split(' ');
+    for (const word of words) {
+      this.displayedMessage += word + ' ';
+      await this.delay(150); // Adjust this value to control the speed of the animation
+    }
+  }
+
+  delay(ms: number) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
 }
