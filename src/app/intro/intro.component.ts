b5a2ddb5e4e2558d64926ac42aefc3f935a3e538
Tomáš Bencko <tomas.bencko@cassoviacode.com>
2023-04-29 22:07:49 +0200

    Add game intro animations – 1st iteration

--- src/app/intro/intro.component.ts
+++ src/app/intro/intro.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { introContent, IntroStep } from './intro-content';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-intro',
   templateUrl: './intro.component.html',
-  styleUrls: ['./intro.component.scss']
+  styleUrls: ['./intro.component.scss'],
+  animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('300ms ease-in', style({ opacity: 1 }))
+      ]),
+      transition(':leave', [
+        style({ opacity: 1 }),
+        animate('300ms ease-out', style({ opacity: 0 }))
+      ])
+    ])
+  ]
 })
 export class IntroComponent {
   introContent: IntroStep[] = introContent;
