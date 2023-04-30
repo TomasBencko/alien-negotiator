61f18f893a54e77df118f8edbe1b693d8ae08bc9
Tomas Bencko <tommyno222@gmail.com>
2023-05-01 00:09:06 +0200

    Polish intro images

--- src/app/intro/intro-content.ts
+++ src/app/intro/intro-content.ts
@@ -7,7 +7,7 @@ export interface IntroStep {
 
 export const introContent: IntroStep[] = [
   {
-    image: 'intro-1E.jpg',
+    image: 'intro-1.jpg',
     text: [
       "Milý ambasádor,",
       "Ak ste sa náhodou práve zobudili z kómy a divíte sa, čo sa to tu všade deje, dovoľte mi ponúknuť vám rýchly prehľad."
@@ -15,49 +15,49 @@ export const introContent: IntroStep[] = [
     btnText: 'Pokračovať'
   },
   {
-    image: 'intro-2B.jpg',
+    image: 'intro-2.jpg',
     text: [
       "Ku našej malej modrej guličke nečakane priletel excentrický mimozemský vodca Zoglorp so svojou vojenskou flotilou, ktorá sa rozprestiera po oblohe ako nevkusný zástup vesmírnych kobyliek."
     ],
     btnText: 'Pokračovať'
   },
   {
-    image: 'intro-3E.jpg',
+    image: 'intro-3.jpg',
     text: [
       "Nikto nevie čo možno od Zoglorpa čakať, ale dovoľte mi povedať, že sa nejaví ako niekto, kto by sa prišiel pozrieť, či máme dostatok pečiva na obedom stole."
     ],
     btnText: 'Pokračovať'
   },
   {
-    image: 'intro-4E.jpg',
+    image: 'intro-4.jpg',
     text: [
       "Zem je v úplnom zúfalstve a zmätok by sa dal krájať svetlomečom. No je čas, aby sme sa dali dokopy a urobili to, čo robíme najlepšie–improvizovali."
     ],
     btnText: 'Pokračovať'
   },
   {
-    image: 'intro-1E.jpg',
+    image: 'intro-1.jpg',
     text: [
       "Práve preto sme sa rozhodli vyslať najlepšieho človeka (týmto človekom ste vy, pre prípad, že by ste si to ešte neuvedomili), aby sa so Zoglorpom pokúsil vyjednať mier."
     ],
     btnText: 'Pokračovať'
   },
   {
-    image: 'intro-2B.jpg',
+    image: 'intro-6.jpg',
     text: [
       "Berte to ako záchrannú akciu. Nájdite spoločnú reč, či už to bude cez pekné slová, absurdné dohody, alebo masáže s aromatickými olejmi—čokoľvek, čo zabezpečí, že naša planéta zostane v jednom kuse."
     ],
     btnText: 'Pokračovať'
   },
   {
-    image: 'intro-7D.jpg',
+    image: 'intro-7.jpg',
     text: [
       "Máme plnú dôveru vo Vaše diplomatické schopnosti. Takže s radosťou vás vysielame na túto neobyčajnú misiu, ktorej úspech je taký neistý ako životnosť žabičky na diaľnici."
     ],
     btnText: 'Pokračovať'
   },
   {
-    image: 'intro-1E.jpg',
+    image: 'intro-1.jpg',
     text: [
       "S úprimným pozdravom a veľkým optimizmom,",
       "Váš prezident"
